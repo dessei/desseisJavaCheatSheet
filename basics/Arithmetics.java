3467ce1604c0ac417809f6df28fc071af3807b7e
dessei <Fritz.Meitner@gmx.de>
2026-03-20 00:47:48 +0100

    arithmetic added to cheat sheet

---
+++ basics/Arithmetics.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Arithmetics {
+    public void arithmet_example() {
+        int x = 6;
+        int y = 7;
+        int z = x + y;
+        z += x;
+        x = z * y;
+        y = x - z;
+        z = x / y;
+        x = z % y;
+        x++;
+        y--;
+        
+        return;
+    }
+    
+}
